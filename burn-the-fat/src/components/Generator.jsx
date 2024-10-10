da9eb8ac521a00c7ca1a098c87c477e5556f5770
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2024-10-10 19:02:31 +0600

    generator updated

--- burn-the-fat/src/components/Generator.jsx
+++ burn-the-fat/src/components/Generator.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import SectionWrapper from './SectionWrapper'
 
 function Header(props) {
+  const {index, title, description} = props
   return (
     <div>
       <div>
-
+        <h4>{title}</h4>
       </div>
+      <p>{description}</p>
     </div>
   )
 }
@@ -14,6 +16,7 @@ function Header(props) {
 export default function Generator() {
   return (
     <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
+      <Header index={'01'} title={"Pick your poison"} description={'Select the workout you wish to endure'}/>
     </SectionWrapper>
   )
 }
