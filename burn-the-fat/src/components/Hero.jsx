5e689d3f443639fc67070fe55e0b065774042498
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2024-10-10 12:57:08 +0600

    hero components finished

--- burn-the-fat/src/components/Hero.jsx
+++ burn-the-fat/src/components/Hero.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center'>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
         <div className='flex flex-col gap-4'>
         <p>IT'S TIME TO GET</p>
         <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>body<span className='text-blue-400'>Shredded</span></h1>
         </div>
         <p className='text-small md:text-base font-light'>I hereby acknowledge that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors</p>
+        <button className='px-8 py-6 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+            <p>Accept & Begin</p>
+        </button>
     </div>
   )
 }
